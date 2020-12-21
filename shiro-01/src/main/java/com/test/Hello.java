3eda10bb1cfd7ed05c9d721025f6815371e8a60e
lx1046 <1046978060@qq.com>
2020-12-21 09:40:06 +0800

    Initial commit

---
+++ shiro-01/src/main/java/com/test/Hello.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author leix
+ * @create 2020-12-17 10:04
+ */
+public class Hello {
+
+    public void main(){
+        //region Description
+        System.out.println("aa");
+        System.out.println("aa");
+        System.out.println("aa");
+        System.out.println("aa");
+        //endregion
+        Date date = new Date();
+        List<String> abc = new ArrayList<>();
+        abc.add(1,"a");
+
+    }
+}
